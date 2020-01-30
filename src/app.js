b240cef4087933c306f91910b790cd483701de88
Agustín Nieto García <anietog1@eafit.edu.co>
2020-01-30 13:35:04 -0500

    Allowing json post

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 // connecting to db
-mongoose.connect('mongodb://localhost/samples-mongo')
-    .then(db => console.log('DB connected'))
+mongoose.connect('mongodb://localhost/samples-mongo', { useUnifiedTopology: true, useNewUrlParser: true})
+    .then(db => console.log('DB CONNECTED'))
     .catch(err => console.log(err));
 
 // importing routes
@@ -19,12 +19,12 @@ app.set('view engine', 'ejs');
 
 // middlewares
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // routes
 app.use('/', indexRoutes);
 
 // server
 app.listen(app.get('port'), () => {
-    console.log(`Server listening on port ${app.get('port')}`);
+    console.log(`SERVER LISTENING ON PORT ${app.get('port')}`);
 });
