029d367e88935edff6748af9a68574d38e8e4195
Agustín Nieto García <anietog1@eafit.edu.co>
2020-02-15 12:51:02 -0500

    Adding apiKey auth before api responses. Changing testing files

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-// samples
 const Sample = require('../models/sample');
+const User = require('../models/user');
 
 router.get('/samples', async (req, res) => {
-    const samples = await Sample.find();
-    res.send(samples);
+    const validApiKey = await User.exists({apiKey: req.body.apiKey});
+
+    if(validApiKey) {
+        const samples = await Sample.find();
+        res.send(samples);
+    } else {
+        res.sendStatus(401);
+    }
 });
 
 router.post('/samples/new', async (req, res) => {
-    const sample = new Sample(req.body);
-    await sample.save();
-    res.sendStatus(200);
+    const validApiKey = await User.exists({apiKey: req.body.apiKey});
+
+    if(validApiKey) {
+        const sample = new Sample(req.body);
+        await sample.save();
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(401);
+    }
 });
 
 module.exports = router;
