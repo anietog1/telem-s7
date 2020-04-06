d49ff559b0912139cc8bb0bc635a6ce2f0cc1765
Agustín Nieto <anietog1@eafit.edu.co>
2020-04-06 12:28:48 -0500

    Resolviendo el problema de dia de accion min-max

---
+++ accion-min-max.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python3
+
+from mrjob.job import MRJob
+from datetime import datetime
+
+class MyMRJob(MRJob):
+    def mapper(self, _, line):
+        company, price, date = line.split(',')
+
+        # Es posible comparar enteros alfanuméricamente y da el
+        # resultado correcto. Sin embargo, estas líneas permiten
+        # ignorar el encabezado del csv
+        try:
+            price = int(price)
+        except ValueError:
+            pass
+        else:
+            yield company, (price, date)
+
+    def reducer(self, company, pricedates):
+        min_price, min_date = next(pricedates)
+        max_price, max_date = min_price, min_date
+
+        for price, date in pricedates:
+            if price < min_price:
+                min_price, min_date = price, date
+            
+            if price > max_price:
+                max_price, max_date = price, date
+
+        yield company, (min_date, max_date)
+
+if __name__ == '__main__':
+    MyMRJob.run()
