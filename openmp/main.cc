52dff776e532ef83a5462ff2be60f7e9aa9c3b8d
evinracher <eevinley@gmail.com>
2020-05-25 16:51:12 -0500

    Adding first version openmp

--- openmp/main.cc
+++ openmp/main.cc
@@ -3,27 +3,45 @@
 #include <omp.h>
 #include <time.h>
 
-void count_nucleotides(size_t nts, const char *dna, unsigned long counts[4]) {
-  for(size_t i = 0; i < nts; ++i) {
-    if(dna[i] == 'A') ++counts[0];
-    if(dna[i] == 'C') ++counts[1];
-    if(dna[i] == 'G') ++counts[2];
-    if(dna[i] == 'T') ++counts[3];
+void count_nucleotides(size_t nts, const char *dna, unsigned long total[4])
+{
+  unsigned long counts[4] = {0UL, 0UL, 0UL, 0UL};
+#pragma omp for
+  for (size_t i = 0; i < nts; ++i)
+  {
+    if (dna[i] == 'A')
+      ++counts[0];
+    if (dna[i] == 'C')
+      ++counts[1];
+    if (dna[i] == 'G')
+      ++counts[2];
+    if (dna[i] == 'T')
+      ++counts[3];
+  }
+
+#pragma omp critical
+  {
+    total[0] += counts[0];
+    total[1] += counts[1];
+    total[2] += counts[2];
+    total[3] += counts[3];
   }
 }
 
-void initialize_buffer(size_t nts, char *dna) {
+void initialize_buffer(size_t nts, char *dna)
+{
   srand(time(NULL));
   char alphabet[4] = {'A', 'C', 'G', 'T'};
-
-  for(size_t i = 0; i < nts; ++i) {
+  for (size_t i = 0; i < nts; ++i)
+  {
     dna[i] = alphabet[rand() % 4];
   }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   const unsigned long nts = 1UL << 25;
-  char *dna = (char*) malloc(sizeof(char) * nts);
+  char *dna = (char *)malloc(sizeof(char) * nts);
   initialize_buffer(nts, dna);
   unsigned long counts[4] = {0UL, 0UL, 0UL, 0UL};
 
