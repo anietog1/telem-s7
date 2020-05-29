5264c5b367e2ed0e94b6df8ff4a9512d7a3c3de0
evinracher <eevinley@gmail.com>
2020-05-29 16:30:32 -0500

    Preliminar MPI version

--- mpi/main.cc
+++ mpi/main.cc
@@ -4,100 +4,66 @@
 #include <mpi.h>
 using namespace std;
 
-const int nTrials = 10;
-const int skipTrials = 3;
-
-double Stats(double &x, double &dx)
-{
-  x /= (double)(nTrials - skipTrials);
-  dx = sqrt(dx / double(nTrials - skipTrials) - x * x);
-}
-
 long double (*f)(long double x) = exp;
 
-long double trapezoid(double x0, double xn, long long n, const int rank, const int nRanks)
+long double trapezoid(double x0, double xn, long long n)
 {
-  const int stepsPerProcess = double(n - 1) / double(nRanks);
-  const int iStart = int(stepsPerProcess * rank);
-  const int iEnd = int(stepsPerProcess * (rank + 1));
-
   long double h = (xn - x0) / n;
-  long double partial_acum = 0.0;
-#pragma omp parallel for reduction(+: partial_acum)
-  for (long long i = iStart; i < iEnd; ++i)
-  {
-    partial_acum += f(x0 + h * i);
-  }
   long double acum = 0.0;
-  MPI_Allreduce(&partial_acum, &acum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+#pragma omp parallel for reduction(+ \
+                                   : acum)
+  for (long long i = 0; i < n; ++i)
+  {
+    acum += f(x0 + h * i);
+  }
   return (h / 2) * (f(x0) + 2 * acum + f(xn));
 }
 
 int main(int argc, char **argv)
 {
-  MPI_Init(&argc, &argv);
+  int n, p, i, j, ierr, num;
+  float result, a, b;
+  float my_a, my_b, my_range;
 
-  int rank, nRanks;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &nRanks);
+  int myid, source, dest, tag;
+  MPI_Status status;
+  float my_result;
 
-  double t, dt, f, df;
+  a = 0.;      /* lower limit of integration */
+  b = 10;      /* upper limit of integration */
+  n = 1000000; /* number of increment within each process */
 
-  // 1 000 000 000
-  long long n = 1000000000;
-  long double total;
+  dest = 0;  /* define the process that computes the final result */
+  tag = 123; /* set the tag to identify this particular job */
 
-  if (rank == 0)
+  /* Starts MPI processes ... */
+  const double t0 = omp_get_wtime();
+  MPI_Init(&argc, &argv);               /* starts MPI */
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid); /* get current process id */
+  MPI_Comm_size(MPI_COMM_WORLD, &p);    /* get number of processes */
+
+  num = n / p; /* number of intervals calculated by each process*/
+  my_range = (b - a) / p;
+  my_a = a + myid * my_range;
+  my_b = my_a + my_range;
+  my_result = trapezoid(my_a, my_b, num);
+  if (myid == 0)
   {
-    printf("\n\033[1mNumerical integration with n=%d\033[0m\n", n);
-    printf("\033[1m%5s %15s %15s\033[0m\n", "Step", "Time, ms", "GSteps/s");
-    fflush(stdout);
-  }
-
-  for (int iTrial = 1; iTrial <= nTrials; iTrial++)
-  {
-
-    double a = double(iTrial - 1);
-    double b = double(iTrial + 1);
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    const double t0 = omp_get_wtime();
-    long double result = trapezoid(a, b, n, rank, nRanks);
-    const double t1 = omp_get_wtime();
-
-    const double ts = t1 - t0;                 // time in seconds
-    const double tms = ts * 1.0e3;             // time in milliseconds
-    const double fpps = double(n) * 1e-9 / ts; // performance in steps/s
-
-    if (iTrial > skipTrials)
-    { // Collect statistics
-      t += tms;
-      dt += tms * tms;
-      f += fpps;
-      df += fpps * fpps;
-    }
-
-    if (rank == 0)
+    result = my_result;
+    for (i = 1; i < p; i++)
     {
-      // Output performance
-      printf("%5d %15.3f %15.3f%s\n",
-             iTrial, tms, fpps, (iTrial <= skipTrials ? "*" : ""));
-      fflush(stdout);
-      MPI_Allreduce(&result, &total, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);      
+      source = i; /* MPI process number range is [0,p-1] */
+      MPI_Recv(&my_result, 1, MPI_REAL, source, tag,
+               MPI_COMM_WORLD, &status);
+      result += my_result;
     }
+    printf("The result =%f\n", result);
+    const double t1 = omp_get_wtime();
+    printf("Time(sec): %f\n", t1 - t0);
   }
-
-  if (rank == 0)
+  else
   {
-    printf("-----------------------------------------------------\n");
-    printf("\033[1m%s\033[0m\n%8s   \033[42m%8.1f+-%.1f\033[0m   \033[42m%8.1f+-%.1f\033[0m\n",
-           "Average performance:", "", t, dt, f, df);
-    printf("-----------------------------------------------------\n");
-    printf("* - warm-up, not included in average\n\n");
-
-    printf("Result: %Lf\n", total);
+    MPI_Send(&my_result, 1, MPI_REAL, dest, tag, MPI_COMM_WORLD); // send my_result to intended dest.
   }
-
-  MPI_Finalize();
+  MPI_Finalize(); /* let MPI finish up ... */
 }
